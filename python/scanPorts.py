a170ad75ae5f958e4839fa74ff9d5fef1b3e7ea5
燕隼 <heyuntao789789@163.com>
2015-05-08 13:51:52 +0800

    a

---
+++ python/scanPorts.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#########################################
+# File Name: scanPorts.py
+# Author: F.L.K
+# Mail: nkuflk@gmail.com
+# Created Time: 2015-05-08 11:29:50
+#########################################
+
+import socket
